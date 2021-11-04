7e5a5dfa1512419b2a43bf5746c8537aa88a7976
AnthonWellsjo <anthonwellsjo@gmail.com>
2021-11-04 16:23:36 +0100

    test

--- components/Misc/BorderAnimations/BorderAnim.tsx
+++ components/Misc/BorderAnimations/BorderAnim.tsx
@@ -13,11 +13,37 @@ const BorderAnim: React.FC<props> = ({ children, zone, scroll }) => {
   const [plates, setPlates] = useState<{ top: Plate[], right: Plate[], bottom: Plate[], left: Plate[] }>({ top: [], right: [], bottom: [], left: [] });
 
   useEffect(() => {
-    setPlates({ top: getAnimPlates(40), right: getAnimPlates(20, 41), bottom: getAnimPlates(40, 62), left: getAnimPlates(20, 103) });
+    setPlates({ top: getAnimPlates(20), right: getAnimPlates(10, 21), bottom: getAnimPlates(20, 32), left: getAnimPlates(10, 53) });
   }, [])
 
   const getTotalNoOfPlates = () => {
     return plates.bottom.length + plates.right.length + plates.top.length + plates.left.length;
+  }
+
+  const getRandomPosition = (axel: "x" | "y") => {
+    if (axel === "x") {
+      switch (Math.floor(Math.random() * 2)) {
+        case 0: return (Math.random() * 400).toString();
+        case 1: return -(Math.random() * 400).toString();
+        default: return (Math.random() * 400).toString();
+      }
+    }
+    if (axel === "y") {
+      switch (Math.floor(Math.random() * 2)) {
+        case 0: return (Math.random() * 400).toString();
+        case 1: return -(Math.random() * 400).toString();
+        default: return (Math.random() * 400).toString();
+      }
+    }
+
+  }
+
+  const getRandomRotation = () => {
+    switch (Math.floor(Math.random() * 2)) {
+      case 0: return (Math.random() * 300).toString();
+      case 1: return -(Math.random() * 300).toString();
+      default: return (Math.random() * 300).toString();
+    }
   }
 
   const thePlateMoveShouldIt = (order: number) => {
@@ -25,7 +51,7 @@ const BorderAnim: React.FC<props> = ({ children, zone, scroll }) => {
     const orderPercentage = order / totPlates * 100;
     const scrollPerc = getPercentageOfZone(zone, scroll);
     // console.log("percentages", orderPercentage, scrollPerc, orderPercentage - scrollPerc);
-    if (Math.abs(orderPercentage - scrollPerc) < 2) return true;
+    if (Math.abs(orderPercentage - scrollPerc) < 1) return true;
     return false;
   }
 
@@ -34,7 +60,7 @@ const BorderAnim: React.FC<props> = ({ children, zone, scroll }) => {
     const orderPercentage = order / totPlates * 100;
     const scrollPerc = getPercentageOfZone(zone, scroll);
     // console.log("percentages", orderPercentage, scrollPerc, orderPercentage - scrollPerc);
-    if ((scrollPerc - orderPercentage) > 0) return true;
+    if ((scrollPerc - orderPercentage) > -.5) return true;
     return false;
   }
 
@@ -42,8 +68,14 @@ const BorderAnim: React.FC<props> = ({ children, zone, scroll }) => {
     plates.top.length,
     plates.top.map(plate => (
       {
-        transform: `rotate(${thePlateMoveShouldIt(plate.order) ? plate.order * getPercentageOfZone(zone, scroll) / 2 : "0"}deg)`,
-        opacity: thePlateIsVisibileIsIt(plate.order) ? 1 : 0
+        transform: thePlateIsVisibileIsIt(plate.order) ? `rotate(${(plate.order / 10).toString()}deg) translateX(0px) translateY(0px)` : `rotate(${getRandomRotation()}deg) translateX(${getRandomPosition("x")}px) translateY(${getRandomPosition("y")}px)`,
+        opacity: thePlateIsVisibileIsIt(plate.order) ? 1 : 0,
+        config: {
+          mass: 45,
+          friction: 150,
+          tension:200
+          
+        }
       }
     )
     )
@@ -52,8 +84,13 @@ const BorderAnim: React.FC<props> = ({ children, zone, scroll }) => {
     plates.right.length,
     plates.right.map(plate => (
       {
-        transform: `rotate(${thePlateMoveShouldIt(plate.order) ? plate.order * getPercentageOfZone(zone, scroll) / 2 : "0"}deg)`,
-        opacity: thePlateIsVisibileIsIt(plate.order) ? 1 : 0
+        transform: thePlateIsVisibileIsIt(plate.order) ? `rotate(${(plate.order / 10).toString()}deg) translateX(0px) translateY(0px)` : `rotate(${getRandomRotation()}deg) translateX(${getRandomPosition("x")}px) translateY(${getRandomPosition("y")}px)`,
+        opacity: thePlateIsVisibileIsIt(plate.order) ? 1 : 0,
+        config: {
+          mass: 45,
+          friction: 150,
+          tension:200
+        }
       }
     )
     )
@@ -62,8 +99,13 @@ const BorderAnim: React.FC<props> = ({ children, zone, scroll }) => {
     plates.bottom.length,
     plates.bottom.map(plate => (
       {
-        transform: `rotate(${thePlateMoveShouldIt(plate.order) ? plate.order * getPercentageOfZone(zone, scroll) / 2 : "0"}deg)`,
-        opacity: thePlateIsVisibileIsIt(plate.order) ? 1 : 0
+        transform: thePlateIsVisibileIsIt(plate.order) ? `rotate(${(plate.order / 10).toString()}deg) translateX(0px) translateY(0px)` : `rotate(${getRandomRotation()}deg) translateX(${getRandomPosition("x")}px) translateY(${getRandomPosition("y")}px)`,
+        opacity: thePlateIsVisibileIsIt(plate.order) ? 1 : 0,
+        config: {
+          mass: 45,
+          friction: 150,
+          tension:200
+        }
       }
     )
     )
@@ -72,8 +114,13 @@ const BorderAnim: React.FC<props> = ({ children, zone, scroll }) => {
     plates.left.length,
     plates.left.map(plate => (
       {
-        transform: `rotate(${thePlateMoveShouldIt(plate.order) ? plate.order * getPercentageOfZone(zone, scroll) / 2 : "0"}deg)`,
-        opacity: thePlateIsVisibileIsIt(plate.order) ? 1 : 0
+        transform: thePlateIsVisibileIsIt(plate.order) ? `rotate(${(plate.order / 10).toString()}deg) translateX(0px) translateY(0px)` : `rotate(${getRandomRotation()}deg) translateX(${getRandomPosition("x")}px) translateY(${getRandomPosition("y")}px)`,
+        opacity: thePlateIsVisibileIsIt(plate.order) ? 1 : 0,
+        config: {
+          mass: 45,
+          friction: 150,
+          tension:200
+        }
       }
     )
     )
@@ -84,32 +131,28 @@ const BorderAnim: React.FC<props> = ({ children, zone, scroll }) => {
       <div style={{ left: 0, top: 0, width: "100%", position: "absolute", display: "flex", justifyContent: "space-evenly" }}>
         {TopSprings.map((styles) => {
           return (
-            <a.div key={styles.transform.id} style={{ width: "1vw", height: "1px", backgroundColor: "black", ...styles }} />
+            <a.div key={styles.transform.id} style={{ transformOrigin: "center", height: "2px", width: "2vw", backgroundColor: "black", ...styles }} />
           )
         })}
-        {/* {platesTop.map((p, i) => {
-          // <h1>hej</h1>
-          return (<a.div key={p.id} style={{ width: "1vw", height: "1px", backgroundColor: "black", transform: `rotate(${styles.rotation(p.order)}deg)` }} />)
-        })} */}
       </div>
       <div style={{ top: 0, right: 0, height: "100%", position: "absolute", display: "flex", flexDirection: "column", justifyContent: "space-evenly" }}>
         {RightSprings.map((styles) => {
           return (
-            <a.div key={styles.transform.id} style={{ height: "1vw", width: "1px", backgroundColor: "black", ...styles }} />
+            <a.div key={styles.transform.id} style={{ transformOrigin: "center", height: "2vw", width: "2px", backgroundColor: "black", ...styles }} />
           )
         })}
       </div>
       <div style={{ left: 0, bottom: 0, width: "100%", position: "absolute", display: "flex", flexDirection: "row-reverse", justifyContent: "space-evenly" }}>
         {BottomSprings.map((styles) => {
           return (
-            <a.div key={styles.transform.id} style={{ width: "1vw", height: "1px", backgroundColor: "black", ...styles }} />
+            <a.div key={styles.transform.id} style={{ transformOrigin: "center", width: "2vw", height: "2px", backgroundColor: "black", ...styles }} />
           )
         })}
       </div>
       <div style={{ top: 0, left: 0, height: "100%", position: "absolute", display: "flex", flexDirection: "column-reverse", justifyContent: "space-evenly" }}>
         {LeftSprings.map((styles) => {
           return (
-            <a.div key={styles.transform.id} style={{ height: "1vw", width: "1px", backgroundColor: "black", ...styles }} />
+            <a.div key={styles.transform.id} style={{ transformOrigin: "center", height: "2vw", width: "2px", backgroundColor: "black", ...styles }} />
           )
         })}
       </div>
